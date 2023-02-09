b0789f7314e5c80f9f318db8912001bc159a457a
agon2022 <116137889+agon2022@users.noreply.github.com>
2023-02-09 15:29:47 +0700

    add lab

---
+++ Project_1651/Dice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    internal class Dice
+    {
+
+        public int Side { get; set; }
+
+        public Dice() { }
+
+        public int Roll()
+        {
+            Random rd = new Random();
+            return rd.Next(1, Side + 1);
+        }
+    }
+}
